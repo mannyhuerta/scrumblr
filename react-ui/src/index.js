f5821d091b623a537146a92e889d859e7aa55113
Manny Huerta <mannyahuerta@gmail.com>
2018-12-15 16:09:22 -0500

    google adsense

--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import queryString from 'query-string'
 import App from './App';
 import stores from './stores'
+require('dotenv').config()
 
 const redirectUri = `${window.location.protocol}//${window.location.hostname}:${window.location.port}${window.location.pathname}`
 const code = queryString.parse(window.location.search).code
